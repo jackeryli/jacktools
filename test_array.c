347d71d028751a94c454b4af0e2a1cb5bec6bc0f
jack <jack.yenchun.li@gmail.com>
2022-04-09 10:56:18 +0800

    array performance

---
+++ test_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 2048 
+
+int main() {
+    
+    struct timespec start, end;
+    double cpu_time_used;
+    int i, j;
+
+    char arr[MAX_SIZE][MAX_SIZE];
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for(i = 0; i < MAX_SIZE; i++) {
+        for(j = 0; j < MAX_SIZE; j++) {
+            arr[j][i] = 0;
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    cpu_time_used = (end.tv_nsec - start.tv_nsec) / 10e9;
+    printf("%f\n", cpu_time_used);
+
+    return 0;
+    
+}
